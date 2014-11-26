596f392e6241d87e5ac16adb04881179d7a8c9ff
Vijay Dhama <vjdhama26@gmail.com>
2014-11-27 00:22:44 +0530

    changed production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Mark8king::Application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.sendgrid.net',
     port:                 587,
-    domain:               'pure-mesa-7668.herokuapp.com',
+    domain:               'mark8king.com',
     user_name:            ENV["SENDGRID_USERNAME"],
     password:             ENV["SENDGRID_PASSWORD"],
     authentication:       'plain',
